a1becea7ad3fd1d2a7eff0e84d78e50fc93d9f29
MamenRC <mamenrueco@gmail.com>
2018-04-21 18:34:27 +0200

    html styles and funcionality

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var init = function() {
+  var carousel = document.getElementById('carousel'),
+      navButtons = document.querySelectorAll('#navigation button'),
+      panelCount = carousel.children.length,
+
+      theta = 0,
+      onNavButtonClick = function( event ){
+        var increment = parseInt( event.target.getAttribute('data-increment') );
+        theta += ( 360 / panelCount ) * increment;
+        carousel.style.transform = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
+console.log("theta: ",theta)
+      };
+
+  for (var i=0; i < 2; i++) {
+    navButtons[i].addEventListener( 'click', onNavButtonClick, false);
+  }
+
+};
+init();
+
+
+// Alert feedback contacto
+var shower = document.getElementById('fd');
+var feedback = document.querySelector('.thanks');
+function showAlert (){
+	 feedback.classList.add('show');
+
+}
+shower.addEventListener('click', showAlert);
